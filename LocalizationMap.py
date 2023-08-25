0bde494ce4490cc0666009c5ec323f141abad65c
SundayMoments <107812745+SundayMoments@users.noreply.github.com>
2023-08-25 00:45:46 -0700

    future support for localization

--- LocalizationMap.py
+++ LocalizationMap.py
@@ -1,4 +1,3 @@
-# Define supported languages
 languages = {
     'English': 'EN',
     'French': 'FR',
@@ -8,5 +7,63 @@ languages = {
     'Italian': 'IT',
     'Turkish': 'TR',
     'Portuguese': 'PT',
-    'Chinese': 'ZH'
+    'Chinese': 'ZH',
+    'Russian': 'RU'
+}
+
+sorted_languages = dict(sorted(languages.items(), key=lambda x: (x[0] != 'English', x[1])))
+languages = sorted_languages
+
+
+# Future localization support
+UI_TRANSLATIONS = {
+    'EN': {
+        'title': "Dice Mod Helper - Support me on Ko-Fi | SeanConnerX0 - {}",
+        'section_handles': "Handles and Folder Name(s)",
+        'folder_name': "Folder Name:",
+        'generate': "Generate",
+        'in_game_info': "In-Game Information",
+        'name': "Name:",
+        'description': "Description:",
+        'mod_uuid': "Mod UUID:",
+        'meta_info': "Meta Information",
+        'author': "Author:",
+        'mod_name': "Mod Name:",
+        'mod_description': "Mod Description:",
+        'output': "Output",
+        'output_directory': "Output Directory:",
+        'browse': "Browse",
+        'build': "Build",
+        'open_output_dir': "Open Output Directory",
+        'open_localization_dir': "Open Localization Directory",
+        'convert_xml_loca': "Convert XML to .loca",
+        'open_dds_dir': "Open .DDS Directory",
+    },
+    'FR': {
+
+    },
+    'DE': {
+
+    },
+    'PL': {
+
+    },
+    'UK': {
+
+    },
+    'IT': {
+
+    },
+    'TR': {
+
+    },
+    'PT': {
+
+    },
+    'ZH': {
+
+    },
+    'RU': {
+
+    }
 }
